c67527edf1de15f4a44c2f62425d0e444592b68b
Lexi Larkin <lexi@qixils.dev>
2023-08-12 03:20:07 -0400

    feat: acknowledge checked effects

--- crowdcontrol_util.lua
+++ crowdcontrol_util.lua
@@ -9,9 +9,12 @@ function cc_send(msg)
     outgoing:push(msg .. "\0")
 end
 
-function cc_isactive(effect)
-    for _, request in ipairs(cc_requests) do
-        if request.code == effect then
+-- Portmaneau of "check" and "acknowledge", this returns true if the effect is
+-- active and marks the effect as acknowledged.
+function cc_chack(effect)
+    for i, request in ipairs(cc_requests) do
+        if request.code == effect and not request.acknowledged then
+            request.acknowledged = true
             return true
         end
     end
